0e86ab7becf0f765882ce4269322bb1f1d9ac7ca
RafaelSL <rafaelsilvadelima37@gmail.com>
2023-05-15 09:01:04 -0300

    changing table to div structure, fixing arrow function problem, adding if cell is empty check, adding clear board when moving piece

--- src/components/grid.js
+++ src/components/grid.js
@@ -17,52 +17,52 @@ function Grid() {
   return (
     <div>
       <div className="grid">
-        <table className='table'>
-          <tr className='row-odd'>
-            <td className={grid[0][0]} onClick={onCellClick(grid, setGrid, phase, setPhase, 0, 0)}></td>
-            <td className={grid[0][1]} onClick={onCellClick(grid, setGrid, phase, setPhase, 0, 1)}></td>
-            <td className={grid[0][2]} onClick={onCellClick(grid, setGrid, phase, setPhase, 0, 2)}></td>
-            <td className={grid[0][3]} onClick={onCellClick(grid, setGrid, phase, setPhase, 0, 3)}></td>
-            <td className={grid[0][4]} onClick={onCellClick(grid, setGrid, phase, setPhase, 0, 4)}></td>
-          </tr>
-          <tr className='row'>
-            <td className={grid[1][0]} onClick={onCellClick(grid, setGrid, phase, setPhase, 1, 0)}></td>
-            <td className={grid[1][1]} onClick={onCellClick(grid, setGrid, phase, setPhase, 1, 1)}></td>
-            <td className={grid[1][2]} onClick={onCellClick(grid, setGrid, phase, setPhase, 1, 2)}></td>
-            <td className={grid[1][3]} onClick={onCellClick(grid, setGrid, phase, setPhase, 1, 3)}></td>
-            <td className={grid[1][4]} onClick={onCellClick(grid, setGrid, phase, setPhase, 1, 4)}></td>
-          </tr>        
-          <tr className='row-odd'>
-            <td className={grid[2][0]} onClick={onCellClick(grid, setGrid, phase, setPhase, 2, 0)}></td>
-            <td className={grid[2][1]} onClick={onCellClick(grid, setGrid, phase, setPhase, 2, 1)}></td>
-            <td className={grid[2][2]} onClick={onCellClick(grid, setGrid, phase, setPhase, 2, 2)}></td>
-            <td className={grid[2][3]} onClick={onCellClick(grid, setGrid, phase, setPhase, 2, 3)}></td>
-            <td className={grid[2][4]} onClick={onCellClick(grid, setGrid, phase, setPhase, 2, 4)}></td>
-          </tr>        
-          <tr className='row'>
-            <td className={grid[3][0]} onClick={onCellClick(grid, setGrid, phase, setPhase, 3, 0)}></td>
-            <td className={grid[3][1]} onClick={onCellClick(grid, setGrid, phase, setPhase, 3, 1)}></td>
-            <td className={grid[3][2]} onClick={onCellClick(grid, setGrid, phase, setPhase, 3, 2)}></td>
-            <td className={grid[3][3]} onClick={onCellClick(grid, setGrid, phase, setPhase, 3, 3)}></td>
-            <td className={grid[3][4]} onClick={onCellClick(grid, setGrid, phase, setPhase, 3, 4)}></td>
-          </tr>        
-          <tr className='row-odd'>
-            <td className={grid[4][0]} onClick={onCellClick(grid, setGrid, phase, setPhase, 4, 0)}></td>
-            <td className={grid[4][1]} onClick={onCellClick(grid, setGrid, phase, setPhase, 4, 1)}></td>
-            <td className={grid[4][2]} onClick={onCellClick(grid, setGrid, phase, setPhase, 4, 2)}></td>
-            <td className={grid[4][3]} onClick={onCellClick(grid, setGrid, phase, setPhase, 4, 3)}></td>
-            <td className={grid[4][4]} onClick={onCellClick(grid, setGrid, phase, setPhase, 4, 4)}></td>
-          </tr>        
-          <tr className='row'>
-            <td className={grid[5][0]} onClick={onCellClick(grid, setGrid, phase, setPhase, 5, 0)}></td>
-            <td className={grid[5][1]} onClick={onCellClick(grid, setGrid, phase, setPhase, 5, 1)}></td>
-            <td className={grid[5][2]} onClick={onCellClick(grid, setGrid, phase, setPhase, 5, 2)}></td>
-            <td className={grid[5][3]} onClick={onCellClick(grid, setGrid, phase, setPhase, 5, 3)}></td>
-            <td className={grid[5][4]} onClick={onCellClick(grid, setGrid, phase, setPhase, 5, 4)}></td>
-          </tr>
-        </table>
+        <div className='table'>
+          <div className='row-odd'>
+            <div className={grid[0][0]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 0, 0)}></div>
+            <div className={grid[0][1]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 0, 1)}></div>
+            <div className={grid[0][2]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 0, 2)}></div>
+            <div className={grid[0][3]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 0, 3)}></div>
+            <div className={grid[0][4]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 0, 4)}></div>
+          </div>
+          <div className='row'>
+            <div className={grid[1][0]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 1, 0)}></div>
+            <div className={grid[1][1]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 1, 1)}></div>
+            <div className={grid[1][2]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 1, 2)}></div>
+            <div className={grid[1][3]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 1, 3)}></div>
+            <div className={grid[1][4]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 1, 4)}></div>
+          </div>        
+          <div className='row-odd'>
+            <div className={grid[2][0]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 2, 0)}></div>
+            <div className={grid[2][1]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 2, 1)}></div>
+            <div className={grid[2][2]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 2, 2)}></div>
+            <div className={grid[2][3]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 2, 3)}></div>
+            <div className={grid[2][4]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 2, 4)}></div>
+          </div>        
+          <div className='row'>
+            <div className={grid[3][0]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 3, 0)}></div>
+            <div className={grid[3][1]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 3, 1)}></div>
+            <div className={grid[3][2]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 3, 2)}></div>
+            <div className={grid[3][3]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 3, 3)}></div>
+            <div className={grid[3][4]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 3, 4)}></div>
+          </div>        
+          <div className='row-odd'>
+            <div className={grid[4][0]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 4, 0)}></div>
+            <div className={grid[4][1]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 4, 1)}></div>
+            <div className={grid[4][2]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 4, 2)}></div>
+            <div className={grid[4][3]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 4, 3)}></div>
+            <div className={grid[4][4]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 4, 4)}></div>
+          </div>        
+          <div className='row'>
+            <div className={grid[5][0]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 5, 0)}></div>
+            <div className={grid[5][1]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 5, 1)}></div>
+            <div className={grid[5][2]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 5, 2)}></div>
+            <div className={grid[5][3]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 5, 3)}></div>
+            <div className={grid[5][4]} onClick={() => onCellClick(grid, setGrid, phase, setPhase, 5, 4)}></div>
+          </div>
+        </div>
       </div>
-      <br /><br /><br /><br /><br />
+      <br /><br /><br />
       <div className='phase-text'>
         {phase}
       </div>
--- src/handlers/click-handler.js
+++ src/handlers/click-handler.js
@@ -1,17 +1,43 @@
-export function onCellClick(grid, setGrid, phase, setPhase, x, y) {
+import { differenceBasedOnDirectionForOdd, differenceBasedOnDirectionForEven } from '../utils/hexagonal-grid-dy-table'
+
+function isNotOutOfBoundaries(y, x) {
+    return true; //CHECK IF OUT OF BOUNDARIES
+}
+
+export function onCellClick(grid, setGrid, phase, setPhase, y, x) {
+    // alert(`x = ${x}, y = ${y}`)
+
     switch (phase) {
         case 'neutral':
             if (grid[y][x] === 'cell-actor') {
-                //spawn range if not obstacle or actor
+                console.log(y, x, grid[y][x])
+                
+                if (y/2 > 0) {
+                    for (const differenceByDirection of differenceBasedOnDirectionForOdd) {
+                        const newY = y-differenceByDirection.dy;
+                        const newX = x-differenceByDirection.dx;
+
+                        if (isNotOutOfBoundaries(newY, newX) && grid[newY][newX] === 'cell-empty') grid[newY][newX] = 'cell-range';
+                    }
+                } else {
+                    for (const differenceByDirection of differenceBasedOnDirectionForEven) {
+                        const newY = y-differenceByDirection.dy;
+                        const newX = x-differenceByDirection.dx;
+
+                        if (isNotOutOfBoundaries(newY, newX) && grid[newY][newX] === 'cell-empty') grid[newY][newX] = 'cell-range';
+                    }
+                }
 
                 setPhase('selected');
             }
             break;
         case 'selected':
             if (grid[y][x] === 'cell-range') {
-                const newGrid = grid;
-
-                //iterate grid to clear range and position cells
+                const newGrid = grid.map((array) => {
+                    return array.map((element) => {
+                        return element = element === 'cell-range' || element === 'cell-actor' ? 'cell-empty' : element
+                    })
+                });
 
                 newGrid[y][x] = 'cell-actor';
 
