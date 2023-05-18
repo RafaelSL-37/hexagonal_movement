831742d563cb2262ed931567f4a28e73a50bb477
RafaelSL <rafaelsilvadelima37@gmail.com>
2023-05-18 10:38:44 -0300

    reverting comment

--- src/utils/spawn-object-on-grid.js
+++ src/utils/spawn-object-on-grid.js
@@ -3,6 +3,6 @@ export function spawnObjectOnGrid(grid, y, x, object, setGrid, setPhase) {
         grid[y][x] = object;
 
         setGrid(grid);
-        // setPhase('neutral');//TODO: CHANGE THIS AS TO UPDATE THE GRID INSTANTLY
+        setPhase('neutral'); //TODO: CHANGE THIS AS TO UPDATE THE GRID INSTANTLY
     }
 }
